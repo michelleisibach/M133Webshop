65569fb1b0a07cc20f5b85e68c208c9612a84026
Michel Leisibach <michel.leisibach@gmail.com>
2020-01-08 14:42:45 +0100

    updated backend server

--- backend/server.ts
+++ backend/server.ts
@@ -7,6 +7,7 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname + '/../frontend/html/index.html'));
 });
 
+app.use("/assets", express.static(path.join(__dirname, "/../frontend/assets")));
 
 app.listen(8080, () => {
     console.log("Server listening on Port 8080")
