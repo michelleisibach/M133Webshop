78bb0ea60e207c87e39119135c777819530e8453
Michel Leisibach <michel.leisibach@gmail.com>
2020-01-08 14:13:56 +0100

    changed ts

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
-var express = require('express');
-var path = require('path');
+"use strict";
+exports.__esModule = true;
+var express = require("express");
+var path = require("path");
 var app = express();
 app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname + '/../frontend/html/index.html'));
