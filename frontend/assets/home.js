4fa3e1163255190bfd7f484c38f522bb6217df60
michelleisibach <50678154+michelleisibach@users.noreply.github.com>
2020-01-13 21:59:30 +0100

    show products on homepage

--- frontend/assets/home.js
+++ frontend/assets/home.js
@@ -1,9 +1,19 @@
-const tbody = document.querySelector("table tbody");
+const tbody = document.getElementById("products");
 
 
 fetch("/api/products")
     .then(r => r.json())
     .then(products => products.forEach(products => {
         console.log(products);
-
+        tbody.innerHTML +=
+            `<div height: 200px;>                 
+                <img src="/assets/images/${products.imageName}"}>    
+                <hr />
+                <div style="margin-bottom: 25px;">
+                <h3 class="producttitle"> ${products.productName} </h3>    
+                </div>
+                <hr />
+                <div style="font-size: 15px; color:red; margin-bottom: 0%;">${products.normalPrice} CHF  </div>
+                <div style="font-size: 15px; color:green; margin-bottom: 0%;">${products.specialOffer} CHF </div>
+            </div>`;
     }));
