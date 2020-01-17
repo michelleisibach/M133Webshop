783cad380114a229824d54a63beaacb8b2edbe4a
michelleisibach <50678154+michelleisibach@users.noreply.github.com>
2020-01-17 21:03:31 +0100

    show subtotal on top right corner

---
+++ frontend/assets/scripts/subtotal.js
@@ -0,0 +1,29 @@
+const cart = document.getElementById('cart');
+const price = document.getElementById('price');
+var sum = 0;
+
+
+getItem();
+
+function getItem() {
+    fetch("/api/warenkorb/get")
+        .then(r => r.json())
+        .then(r => {
+            let productId = []
+            for (let i = 0; i < r.allProducts.length; i++) {
+                productId.push(r.allProducts[i].id)
+                sum = sum + r.allProducts[i].specialOffer
+
+
+            }
+            displayCheckout();
+
+        });
+}
+
+function displayCheckout() {
+
+    document.getElementById("checkout").innerHTML = `${sum.toFixed(2)}.-`;
+    document.getElementById("pay").innerHTML = `Bezahlen`;
+
+}
